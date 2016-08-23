d24940d76ed853ce037e425fd9dc31413c1510a1
Albertus82 <albertus82@gmail.com>
2016-08-23 21:29:25 +0200

    HTTPS classes.

---
+++ src/main/java/it/albertus/router/client/http/DummyTrustManager.java
@@ -0,0 +1,20 @@
+package it.albertus.router.client.http;
+
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.X509TrustManager;
+
+public class DummyTrustManager implements X509TrustManager {
+
+	@Override
+	public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+		return null;
+	}
+
+	@Override
+	public void checkClientTrusted(final X509Certificate[] certs, final String authType) {}
+
+	@Override
+	public void checkServerTrusted(final X509Certificate[] certs, final String authType) {}
+
+}
---
+++ src/main/java/it/albertus/router/client/http/RouterLoggerHostnameVerifier.java
@@ -0,0 +1,24 @@
+package it.albertus.router.client.http;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+public class RouterLoggerHostnameVerifier implements HostnameVerifier {
+
+	private final String hostname;
+
+	public RouterLoggerHostnameVerifier(final String hostname) {
+		this.hostname = hostname;
+	}
+
+	@Override
+	public boolean verify(final String hostname, final SSLSession session) {
+		if (hostname.equals(this.hostname)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
