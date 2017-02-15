0258d581ac3108a9d7b9e8c9ab98814c11c22317
Alberto <albertus82@gmail.com>
2017-02-15 09:30:30 +0100

    Configuration: log level as String (INFO) instead of int (700).

--- src/main/java/it/albertus/router/client/gui/preference/page/GeneralPreferencePage.java
+++ src/main/java/it/albertus/router/client/gui/preference/page/GeneralPreferencePage.java
@@ -2,7 +2,6 @@ package it.albertus.router.client.gui.preference.page;
 
 import java.util.Locale;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.logging.Level;
 
 import it.albertus.jface.preference.LocalizedLabelsAndValues;
@@ -46,8 +45,8 @@ public class GeneralPreferencePage extends RestartHeaderPreferencePage {
 	public static StaticLabelsAndValues getLoggingComboOptions() {
 		final Map<Integer, Level> levels = LoggingSupport.getLevels();
 		final StaticLabelsAndValues options = new StaticLabelsAndValues(levels.size());
-		for (final Entry<Integer, Level> entry : levels.entrySet()) {
-			options.put(entry.getValue().getName(), entry.getKey().toString());
+		for (final Level level : levels.values()) {
+			options.put(level.getName(), level.getName());
 		}
 		return options;
 	}
