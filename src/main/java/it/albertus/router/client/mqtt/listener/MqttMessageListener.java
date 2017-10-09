5efc81c9c5f583a951af9bf5313cb866a16bfa72
Alberto <albertus82@gmail.com>
2017-10-09 23:11:47 +0200

    Updated jface-utils.

--- src/main/java/it/albertus/router/client/mqtt/listener/MqttMessageListener.java
+++ src/main/java/it/albertus/router/client/mqtt/listener/MqttMessageListener.java
@@ -1,19 +1,19 @@
 package it.albertus.router.client.mqtt.listener;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import it.albertus.mqtt.MqttPayloadDecoder;
-import it.albertus.mqtt.MqttUtils;
 
 public abstract class MqttMessageListener implements IMqttMessageListener {
 
 	private final MqttPayloadDecoder decoder = new MqttPayloadDecoder();
 
 	public String decode(final MqttMessage message) throws IOException {
-		return new String(decoder.decode(message.getPayload()), MqttUtils.CHARSET_UTF8);
+		return new String(decoder.decode(message.getPayload()), StandardCharsets.UTF_8);
 	}
 
 }
